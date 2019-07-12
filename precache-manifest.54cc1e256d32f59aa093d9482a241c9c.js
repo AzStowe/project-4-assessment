37d8e464a61f8e14e706b3d4d20c752f8d738e14
AzStowe <aaron.z.stowe@gmail.com>
2019-07-12 16:42:42 -0500

    Updates

---
+++ precache-manifest.54cc1e256d32f59aa093d9482a241c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620b34403c5a87bb9e05ee5dfd689841",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/index.html"
+  },
+  {
+    "revision": "21d0310eba31b487a900",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/css/main.2478e8b7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "21d0310eba31b487a900",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/main.6841a33f.chunk.js"
+  },
+  {
+    "revision": "d5f08aa8dc1074a3a93f",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/runtime~main.51c75cd6.js"
+  }
+]);
